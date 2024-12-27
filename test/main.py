e0c2ad0c097dbd05abad4850adb0feb10478ad42
barteklukasik <blukasik1904@gmail.com>
2024-12-27 21:08:45 +0100

    Update main.py

--- test/main.py
+++ test/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from backend import get_related_words, get_similar_genres, sp, save_tracks, search_songs
 app = Flask(__name__)
 
 @app.route("/home")
@@ -12,18 +12,39 @@ def home():
 def preset():
     return render_template("preset.html") 
 
-@app.route("/custom")
-def custom():
-    return render_template("custom.html") 
+@app.route("/create")
+def create():
+    return render_template("create.html") 
 
 @app.route("/custom1")
 def custom1():
     return render_template("custom1.html") 
 
-@app.route("/output")
-def output():
-    return render_template("output.html") 
+@app.route('/create_playlist', methods=['POST'])
+def create_playlist():
+    # Pobierz dane z formularza
+    keywords = request.form['keywords']
+    genre = request.form['genre']
+    length = request.form['length']
 
+    # Przetwórz dane
+    keyword_list = [k.strip() for k in keywords.split(',')]
+    genres = get_similar_genres(genre) if genre else None
+    lengthin = int(length)
+
+    try:
+        # Wyszukaj utwory
+        track_ids = search_songs(sp, seed_words=keyword_list, genres=genres, min_length=lengthin)
+
+        # Zapisz playlistę na Spotify
+        playlist_name = "User Generated Playlist"
+        save_tracks(sp, track_ids, playlist_name=playlist_name)
+
+        return render_template ("congrats.html") #f"Playlist '{playlist_name}' created successfully with {len(track_ids)} tracks!"
+    except ValueError as ve:
+        return f"Error: {ve}"
+    except Exception as e:
+        return f"Unexpected error: {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)
